afd949ee676bc97d9bda12b7f4647448edbd4fa2
GuadalupeAlzugaray <110639935+GuadalupeAlzugaray@users.noreply.github.com>
2022-08-06 14:14:36 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+var PostDeInstagram: string[] = [
+  "Abrir la App de Instagram"
+  "Ir a mi perfil"
+  "Hacer click en crear"
+  "Seleccionar publicación"
+  "Selecionar la foto que deseo subir"
+  "Hacer clcik en siguiente"
+  "Elegir el filtro"
+  "Editar la foto"
+  "Hacer click en siguiente"
+  "Escribir el pie de foto"
+  "Hacer click en el tilde."
+];
+for (var i = 0; i < PostDeInstagram.length; i++) {
+  console.log("Paso:" + [i + 1], PostDeInstagram[i]);
+
+}
